d56b37612e92e5500dd69de0b345566a8dff3cb5
06ED <hsbest14@yandex.ru>
2024-05-13 19:21:43 +0300

    some changes

--- public/scripts/animation.ts
+++ public/scripts/animation.ts
@@ -1,30 +1,48 @@
-const opacityObserver = new IntersectionObserver(entries => entries.forEach(entry => {
-    if (entry.isIntersecting) {
+const opacityObserver = new IntersectionObserver((entries) =>
+  entries.forEach(
+    (entry) => {
+      if (entry.isIntersecting) {
         entry.target.classList.add("opacity-animate");
         return;
-    }
-    entry.target.classList.remove("opacity-animate");
-}, {threshold: 0.5}));
+      }
+      entry.target.classList.remove("opacity-animate");
+    },
+    { threshold: 0.5 },
+  ),
+);
 
-const rightObserver = new IntersectionObserver(entries => entries.forEach(entry => {
-    if (entry.isIntersecting) {
+const rightObserver = new IntersectionObserver((entries) =>
+  entries.forEach(
+    (entry) => {
+      if (entry.isIntersecting) {
         entry.target.classList.add("right-animate");
         return;
-    }
-    entry.target.classList.remove("right-animate");
-}, {threshold: 0.5}));
+      }
+      entry.target.classList.remove("right-animate");
+    },
+    { threshold: 0.5 },
+  ),
+);
 
-const leftObserver = new IntersectionObserver(entries => entries.forEach(entry => {
-    if (entry.isIntersecting) {
+const leftObserver = new IntersectionObserver((entries) =>
+  entries.forEach(
+    (entry) => {
+      if (entry.isIntersecting) {
         entry.target.classList.add("left-animate");
         return;
-    }
-    entry.target.classList.remove("left-animate");
-}, {threshold: 0.5}));
+      }
+      entry.target.classList.remove("left-animate");
+    },
+    { threshold: 0.5 },
+  ),
+);
 
-document.querySelectorAll(".opacity-animation")
-    .forEach(selector => opacityObserver.observe(selector));
-document.querySelectorAll(".right-animation")
-    .forEach(selector => rightObserver.observe(selector));
-document.querySelectorAll(".left-animation")
-    .forEach(selector => leftObserver.observe(selector));
+document
+  .querySelectorAll(".opacity-animation")
+  .forEach((selector) => opacityObserver.observe(selector));
+document
+  .querySelectorAll(".right-animation")
+  .forEach((selector) => rightObserver.observe(selector));
+document
+  .querySelectorAll(".left-animation")
+  .forEach((selector) => leftObserver.observe(selector));
