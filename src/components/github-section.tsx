95c7e2b2017d0c89e569984270e4a38707d2c292
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-06-25 13:30:35 -0600

    chore: Hide scrollbar and remove stock text from github card

--- src/components/github-section.tsx
+++ src/components/github-section.tsx
@@ -65,29 +65,6 @@ const GithubSection = () => {
               </a>
             </li>
           </ul>
-          <div>
-            <a
-              href="#"
-              className="inline-flex items-center text-xs font-normal text-black hover:underline"
-            >
-              <svg
-                className="w-3 h-3 me-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M7.529 7.988a2.502 2.502 0 0 1 5 .191A2.441 2.441 0 0 1 10 10.582V12m-.01 3.008H10M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-              Why do I need to connect with my wallet?
-            </a>
-          </div>
         </div>
       </div>
     </section>
